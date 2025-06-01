018833e44711ce6197b40d4575f59b476f6e231d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-01 18:57:16 +0800

    feat: update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,33 @@
-import { Box } from '@radix-ui/themes'
+// import { Box } from '@radix-ui/themes'
 
+import { gray, mint } from '@radix-ui/colors'
+
+import DotGrid from './reactbits/DotGrid/DotGrid'
 import Lanyard from './reactbits/Lanyard/Lanyard'
+import TextPressure from './reactbits/TextPressure/TextPressure'
 
 export const Hero = () => (
-  <Box className="h-screen w-screen" style={{ background: 'var(--gray-a2)' }}>
+  <div className="h-screen w-screen children:absolute children:h-screen children:w-screen" style={{ backgroundColor: 'var(--gray-a2)' }}>
+    <DotGrid
+      activeColor={mint.mint9}
+      baseColor={gray.gray3}
+      dotSize={5}
+      gap={50}
+    />
+    <div className="flex flex-col justify-center! items-center my-auto">
+      <TextPressure
+        italic={true}
+        minFontSize={192}
+        text="does kindness plus"
+        textColor={gray.gray12}
+      />
+      <TextPressure
+        italic={true}
+        minFontSize={192}
+        text="sadness equal to zero?"
+        textColor={gray.gray12}
+      />
+    </div>
     <Lanyard gravity={[0, -40, 0]} position={[0, 0, 20]} />
-  </Box>
+  </div>
 )
