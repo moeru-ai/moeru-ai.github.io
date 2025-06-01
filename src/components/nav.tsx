3f771a148cc453f52897942f6f731608831316a7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-06-01 19:36:43 +0800

    chore: update navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '@iconify/react'
-import { Box, IconButton, Text } from '@radix-ui/themes'
+import { Box, IconButton, Text, Tooltip } from '@radix-ui/themes'
 
 export const Nav = () => {
   const navigate = (url: string) => () => window.open(url, '_blank', 'noopener')
@@ -9,15 +9,26 @@ export const Nav = () => {
       <Box className="w-full flex p-2 lg:p-4 gap-2 justify-center items-center">
         <Text className="font-doto leading-none" size="6">MOERU AI</Text>
         <Box className="grow" />
-        <IconButton className="hover:cursor-pointer" data-test-id="npm" onClick={navigate('https://npmjs.com/org/moeru-ai')} variant="soft">
-          <Icon icon="simple-icons:npm" />
-        </IconButton>
-        <IconButton className="hover:cursor-pointer" data-test-id="huggingface" onClick={navigate('https://huggingface.com/moeru-ai')} variant="soft">
-          <Icon icon="simple-icons:huggingface" />
-        </IconButton>
-        <IconButton className="hover:cursor-pointer mr-0" data-test-id="github" onClick={navigate('https://github.com/moeru-ai')} variant="soft">
-          <Icon icon="simple-icons:github" />
-        </IconButton>
+        <Tooltip content="Blog">
+          <IconButton className="hover:cursor-pointer" data-test-id="blog" onClick={navigate('https://blog.moeru.ai')} variant="soft">
+            <Icon icon="simple-icons:rss" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip content="NPM">
+          <IconButton className="hover:cursor-pointer" data-test-id="npm" onClick={navigate('https://npmjs.com/org/moeru-ai')} variant="soft">
+            <Icon icon="simple-icons:npm" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip content="Hugging Face">
+          <IconButton className="hover:cursor-pointer" data-test-id="huggingface" onClick={navigate('https://huggingface.com/moeru-ai')} variant="soft">
+            <Icon icon="simple-icons:huggingface" />
+          </IconButton>
+        </Tooltip>
+        <Tooltip content="GitHub">
+          <IconButton className="hover:cursor-pointer mr-0" data-test-id="github" onClick={navigate('https://github.com/moeru-ai')} variant="soft">
+            <Icon icon="simple-icons:github" />
+          </IconButton>
+        </Tooltip>
       </Box>
     </Box>
   )
