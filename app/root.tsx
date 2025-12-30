55bf0b8c8249596b1fd81dd30a883a76d9d3c7c2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-30 23:42:09 +0800

    chore: update css import

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   ScrollRestoration,
 } from 'react-router'
 
-import './app.css'
+import 'uno.css'
 
 export const links: Route.LinksFunction = () => [
   { href: 'https://fonts.googleapis.com', rel: 'preconnect' },
@@ -17,10 +17,6 @@ export const links: Route.LinksFunction = () => [
     crossOrigin: 'anonymous',
     href: 'https://fonts.gstatic.com',
     rel: 'preconnect',
-  },
-  {
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-    rel: 'stylesheet',
   },
 ]
 
