607a6eb29d2aebfa0f00af2a109c89504af69c4c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-30 22:31:06 +0800

    chore: re-init

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
-import unocss from '@unocss/vite'
-import react from '@vitejs/plugin-react'
-// import { fontless } from 'fontless'
-import { defineConfig } from 'vite'
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'esnext',
-    },
-  },
-  plugins: [
-    react({ babel: { plugins: [['babel-plugin-react-compiler', { target: '19' }]] } }),
-    // https://github.com/unjs/fontaine/tree/main/packages/fontless#configuration
-    // fontless(),
-    unocss(),
-  ],
-})
+  plugins: [tailwindcss(), reactRouter() as any, tsconfigPaths()],
+});
