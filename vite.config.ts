cc115ff1c224e1c2d412adcad8cc85641472e3c0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-30 23:02:11 +0800

    chore: add fontless

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { reactRouter } from '@react-router/dev/vite'
+import { fontless } from 'fontless'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter() as any, tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter() as any,
+    tsconfigPaths(),
+    // https://github.com/unjs/fontaine/tree/main/packages/fontless#configuration
+    fontless(),
+  ],
 })
