b3698124bec3acbb08d5c9b152fd615056b944ae
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-12-31 21:41:40 +0800

    chore: update head

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,8 +5,9 @@ import { Welcome } from '../welcome/welcome'
 const Home = () => <Welcome />
 
 export const meta = (_: Route.MetaArgs) => [
-  { title: 'New React Router App' },
-  { content: 'Welcome to React Router!', name: 'description' },
+  { title: 'Moeru AI' },
+  { content: 'does kindness plus sadness equal to zero?', name: 'description' },
+  { href: 'https://github.com/moeru-ai.png', rel: 'icon', type: 'image/png' },
 ]
 
 export default Home
