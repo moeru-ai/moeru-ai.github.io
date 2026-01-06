31cae5278913aca9abd978a96f192b54fe76d266
藍+85CD <50108258+kwaa@users.noreply.github.com>
2026-01-06 21:24:41 +0800

    chore: update

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,12 +2,16 @@
 
 import type { Mesh } from 'three'
 
+import { useGSAP } from '@gsap/react'
 import { Box, Container, Flex, Heading, Section, Text } from '@radix-ui/themes'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { CustomEase } from 'gsap/CustomEase'
+import { SplitText } from 'gsap/SplitText'
+import { useEffect, useRef, useState } from 'react'
 
-import * as Scrollytelling from '@bsmnt/scrollytelling'
+// import * as Scrollytelling from '@bsmnt/scrollytelling'
 
 const BoxMesh = () => {
   const ref = useRef<Mesh>(null!)
@@ -22,37 +26,77 @@ const BoxMesh = () => {
   )
 }
 
-export const Hero = () => (
-  <Scrollytelling.Root>
-    <Scrollytelling.Pin childHeight="0" pinSpacerHeight="100vh" top={0}>
-      <Flex align="center" className="min-h-screen w-screen" justify="center">
-        <Section size="3">
-          <Container>
-            <Flex align="center" direction={{ initial: 'column', md: 'row' }} gap="6">
-              <Box flexGrow="1" style={{ maxWidth: 500 }}>
-                <Heading mb="4" size="9">
-                  <Text>does kindness plus sadness equal to</Text>
-                  <br />
-                  <Text color="teal" highContrast>zero?</Text>
-                </Heading>
-                <Text className="font-serif">
-                  Moeru AI builds applications, open-source libraries,
-                  <br />
-                  and open-weight models to make AI a bit more “Moe”.
-                </Text>
-              </Box>
-              <Box className="rounded-[--radius-6] bg-[--gray-a2] h-[400px] w-full relative overflow-hidden">
-                <Canvas camera={{ fov: 45, position: [3, 3, 3] }}>
-                  <ambientLight intensity={1.5} />
-                  <pointLight position={[10, 10, 10]} />
-                  <BoxMesh />
-                  <OrbitControls enableZoom={false} />
-                </Canvas>
-              </Box>
-            </Flex>
-          </Container>
-        </Section>
-      </Flex>
-    </Scrollytelling.Pin>
-  </Scrollytelling.Root>
-)
+export const Hero = () => {
+  const divRef = useRef<HTMLDivElement>(null)
+  const headingRef = useRef<HTMLHeadingElement>(null)
+  const splitText = useRef<SplitText>(null)
+  const [headingHidden, setHeadingHidden] = useState(true)
+
+  useGSAP(() => {
+    CustomEase.create('osmo-ease', '0.625, 0.05, 0, 1')
+
+    splitText.current = new SplitText(headingRef.current, {
+      linesClass: 'line',
+      mask: 'words',
+      type: 'lines,words',
+      wordsClass: 'word',
+    })
+
+    gsap.set('.line', { overflow: 'hidden' })
+  }, { scope: divRef })
+
+  const { contextSafe } = useGSAP({ scope: divRef })
+
+  const animate = contextSafe(() => {
+    setHeadingHidden(false)
+
+    gsap.fromTo(headingRef.current!.querySelectorAll('.word'), { yPercent: 110 }, {
+      duration: 0.6,
+      ease: 'osmo-ease',
+      overwrite: true,
+      stagger: 0.06,
+      yPercent: 0,
+    })
+  })
+
+  useEffect(() => animate(), [animate])
+
+  return (
+  // <Scrollytelling.Root>
+  //   <Scrollytelling.Pin childHeight="0" pinSpacerHeight="100vh" top={0}>
+    <Flex align="center" className="min-h-screen w-screen" justify="center">
+      <Section size="3">
+        <Container>
+          <Flex align="center" direction={{ initial: 'column', md: 'row' }} gap="6">
+            <Box flexGrow="1" style={{ maxWidth: 500 }}>
+              <Heading className="whitespace-nowrap" hidden={headingHidden} mb="4" ref={headingRef} size="9">
+                <Text>does kindness</Text>
+                <br />
+                <Text>plus sadness</Text>
+                <br />
+                <Text>equal to</Text>
+                <br />
+                <Text color="teal" highContrast>zero?</Text>
+              </Heading>
+              <Text className="font-serif">
+                Moeru AI builds applications, open-source libraries,
+                <br />
+                and open-weight models to make AI a bit more “Moe”.
+              </Text>
+            </Box>
+            <Box className="rounded-[--radius-6] bg-[--gray-a2] h-[400px] w-full relative overflow-hidden">
+              <Canvas camera={{ fov: 45, position: [3, 3, 3] }}>
+                <ambientLight intensity={1.5} />
+                <pointLight position={[10, 10, 10]} />
+                <BoxMesh />
+                <OrbitControls enableZoom={false} />
+              </Canvas>
+            </Box>
+          </Flex>
+        </Container>
+      </Section>
+    </Flex>
+  //   </Scrollytelling.Pin>
+  // </Scrollytelling.Root>
+  )
+}
